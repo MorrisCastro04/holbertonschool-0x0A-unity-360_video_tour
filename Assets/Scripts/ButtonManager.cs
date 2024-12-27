0842a15cf748a05bd2f63c003fe613aa8c93a4e7
Morris Jomar <jomarcastro2004@gmail.com>
2024-12-26 21:34:32 -0400

    add a fade animation between videos

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -6,11 +6,20 @@ public class ButtonManager : MonoBehaviour
 {
     public GameObject OldPlace;
     public GameObject Camara;
+    public Animator anim;
 
     public void ChangePlace(GameObject NewPlace)
     {
+        StartCoroutine(ChangePlaceFade(NewPlace));
+    }
+
+    IEnumerator ChangePlaceFade(GameObject NewPlace)
+    {
+        anim.SetTrigger("StartFade");
+        yield return new WaitForSeconds(1.5f);
         NewPlace.SetActive(true);
         OldPlace.SetActive(false);
         Camara.transform.position = NewPlace.transform.position;
+        anim.SetTrigger("Fade");
     }
 }
